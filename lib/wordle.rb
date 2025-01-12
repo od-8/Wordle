a6079998d5a0efae5e4ea7594f25f24c97bb38e1
od-8 <od30338@gmail.com>
2025-01-12 20:42:18 +0000

    will look into this more tmrw, cant get working now

--- lib/wordle.rb
+++ lib/wordle.rb
@@ -14,7 +14,29 @@ class Board
   def initialize(correct_word)
     @board = Array.new(6) { Array.new(5, '-') }
     @correct_combo = correct_word.split('')
+    @letter_count = Hash.new(0)
+    @combo_count = Hash.new(0)
+    @hash = Hash.new(0)
     @w = 0
+  end
+
+  def counter(guess_word)
+    current_guess = guess_word.split('')
+    current_guess.each_with_index do |item, _index|
+      @letter_count[item] = current_guess.count(item)
+    end
+
+    @correct_combo.each_with_index do |item, _index|
+      @combo_count[item] = @correct_combo.count(item)
+    end
+    p @letter_count
+    p @combo_count
+
+    @hash = @letter_count.merge(@combo_count) do |key, old_value, new_value|
+      old_value + new_value
+    end
+
+    p @hash
   end
 
   def move(guess_word)
@@ -25,11 +47,11 @@ class Board
   def print_board
     puts ''
     puts '+---+---+---+---+---+'
-    @board.each_with_index do |row, _i|
+    @board.each do |row|
       row.each_with_index do |item, index|
         if @correct_combo[index] == item
           print "| #{item.colorize(:green)} "
-        elsif @correct_combo.include?(item) && @correct_combo[index] != item
+        elsif @correct_combo.include?(item) && @correct_combo[index] != item && @hash.each_value { |i| i <= 2}
           print "| #{item.colorize(:yellow)} "
         else
           print "| #{item} "
@@ -73,6 +95,7 @@ until $game_over == 1
   puts ''
 
   board.move(guess_word)
+  board.counter(guess_word)
   board.print_board
   board.winner?
   board.full?
